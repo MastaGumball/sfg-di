b96ba425f2c8049a99d45068ea62c55d5a9803c9
IbrahimABU55 <ely.ibrahim@gmail.com>
2021-01-08 17:34:54 +0100

    Dependency injection broad view

---
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+
+    @Autowired
+    @Qualifier("propertyGreetingService")
+    public GreetingService greetingService;
+
+    public String getGretting() {
+        return greetingService.sayGreeting();
+    }
+
+
+}
---
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public String getGretting() {
+        return greetingService.sayGreeting();
+    }
+
+    @Autowired
+    @Qualifier("setterGreetingService")
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
